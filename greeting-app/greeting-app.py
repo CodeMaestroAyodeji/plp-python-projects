053e4c930edf0ad623b1024d5aa473752fa36926
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-11-13 02:23:00 +0100

    Commiting my Greeting app

---
+++ greeting-app/greeting-app.py
@@ -0,0 +1,7 @@
+# Personalized Greeting App
+def greet_user():
+    name = input("What is your name? ")
+    color = input("What is your favorite color? ")
+    print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
+
+greet_user()
